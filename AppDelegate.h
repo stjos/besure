8be11a8b55a0ce29c92eacbcc12e9d74ce22fb34
stjos <133094710+stjos@users.noreply.github.com>
2023-05-11 10:59:49 +0800

    v1.0

---
+++ AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  AllTheHere
+//
+//  Created by Hong on 4/8/13.
+//  Copyright (c) 2013 CFCA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
